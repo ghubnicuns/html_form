fd6a76b035a61bff85e3c7fd593e3594379cdfcf
ghubnicuns <ccunan23-0327@cca.edu.ph>
2025-02-26 19:36:41 +0800

    Update action_page.php

--- action_page.php
+++ action_page.php
@@ -1,16 +1,33 @@
 <?php
     var_dump($_POST);
+
+    $fname = '';
+    $gender = '';
+    $country = '';
 ?>
 
 <?php
     $servername = "localhost";
     $username = "root";
     $password = "";
+    $database = "i201";
 
-    $conn = new mysqli($servername, $username, $password);
+    $conn = new mysqli($servername, $username, $password, $database);
+
 
     if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
     }
     echo "Connected successfully";
+?>
+
+<?php
+    $sql = "INSERT INTO student_record (id, fname, gender, country)
+    VALUES (NULL, '$fname', '$gender', '$country')";
+    
+    if ($conn->query($sql) === TRUE) {
+      echo "New record created successfully";
+    } else {
+      echo "Error: " . $sql . "<br>" . $conn->error;
+    }
 ?>
